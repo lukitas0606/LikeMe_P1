336622606c2453bc7183bd436d42c2ed9b2687db
lukitas0606 <nicolascontrerasuribe0@gmail.com>
2024-01-28 04:15:23 -0300

    Desafio modificado

--- LikeMe_Backend/utils/pg.js
+++ LikeMe_Backend/utils/pg.js
@@ -1,7 +1,8 @@
-const dotenv = require('dotenv')
-const { Pool } = require('pg')
+const dotenv = require('dotenv');
+const { Pool } = require('pg');
+const { v4: uuidv4 } = require('uuid');
 
-dotenv.config()
+dotenv.config();
 
 const pool = new Pool({
     user: process.env.PG_USER,
@@ -10,25 +11,32 @@ const pool = new Pool({
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
     allowExitOnIdle: true
-})
+});
 
-const getPost = async (titulo, url, descripcion) => {
-    const consulta = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3) RETURNING *"
-    const values = [ titulo, url, descripcion]
-    const result = await pool.query(consulta, values)
-    console.log(result)
-    return result
-}
+const getPost = async (titulo, img, descripcion) => {
+    const postId = uuidv4();
+    const consulta = "INSERT INTO posts VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [postId, titulo, img, descripcion, null];
+
+    try {
+        const result = await pool.query(consulta, values);
+        console.log(result.rows[0]);
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error al insertar el post:', error);
+        throw error;
+    }
+};
 
 const obtenerPost = async () => {
     try {
-        const { rows } = await pool.query("SELECT * FROM posts")
-        console.log(rows)
-        return rows
+        const { rows } = await pool.query("SELECT * FROM posts");
+        console.log(rows);
+        return rows;
     } catch (error) {
-        console.error('Error al obtener los registros:', error)
-        throw error
+        console.error('Error al obtener los registros:', error);
+        throw error;
     }
-}
+};
 
-module.exports = { getPost, obtenerPost }
+module.exports = { getPost, obtenerPost };
