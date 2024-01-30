671e35fb28cab14aea1a393674d873aacbfbaa29
lukitas0606 <nicolascontrerasuribe0@gmail.com>
2024-01-30 18:37:17 -0300

    Modificacion

--- LikeMe_Backend/server/index.js
+++ LikeMe_Backend/server/index.js
@@ -1,24 +1,26 @@
 const express = require('express')
-const { getPost, obtenerPost } = require('../utils/pg.js')
 const cors = require('cors')
+const { getPost, obtenerPost } = require('../utils/pg.js')
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
-app.listen(3000, console.log("¡Servidor OK!"))
+
 app.get("/posts", async (req, res) => {
 const posteos = await obtenerPost()
 res.json(posteos)
 })
 
 app.post("/posts", async (req, res) => {
-    const { titulo, url, descripcion } = req.body
+    const { id, titulo, url, descripcion } = req.body
     try {
-        await getPost(titulo, url, descripcion)
+        await getPost(id, titulo, url, descripcion)
         res.status(201).json({ mensaje: 'Se ha agregado con éxito' })
     } catch (error) {
         console.error('Error al agregar la publicación:', error)
         res.status(500).json({ error: 'Error interno del servidor' })
     }
 })
+
+app.listen(3000, console.log("¡Servidor OK!"))
